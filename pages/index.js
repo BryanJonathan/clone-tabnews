296434b58670fff315b2b79d66430b2a2507a948
Jonathan Bryan <bryan.jonathan97@gmail.com>
2025-10-28 22:05:56 -0300

    construction page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div
+      style={{
+        height: "98vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>PAGE UNDER CONSTRUCTION</h1>
+      <img style={{ width: 100, height: 100 }} src={"image.png"} alt="asd" />
+      <p>We are working hard to bring you this page soon!</p>
+      <p>Why don't you take a look at my github or linkedin in the meantime?</p>
+      <a target="_blank" href="https://github.com/BryanJonathan">
+        GITHUB
+      </a>
+      <a target="_blank" href="https://www.linkedin.com/in/jonathan-bryan-ca/">
+        LINKEDIN
+      </a>
+    </div>
+  );
 }
 
 export default Home;

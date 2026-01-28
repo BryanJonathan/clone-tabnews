5258f823b060cf4fb028da071c3102d6ac8cb301
Bryan Andrade <bryan@blings.io>
2026-01-27 23:21:47 -0300

    fix eslint files

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 function Home() {
   return (
     <div
@@ -10,13 +12,13 @@ function Home() {
       }}
     >
       <h1>PAGE UNDER CONSTRUCTION</h1>
-      <img
+      <Image
         style={{ width: 100, height: 100 }}
         src={"image.png"}
         alt="worker image"
       />
       <p>We are working hard to bring you this page soon!</p>
-      <p>Why don't you take a look at my github or linkedin in the meantime?</p>
+      <p>Why dont you take a look at my github or linkedin in the meantime?</p>
       <a target="_blank" href="https://github.com/BryanJonathan">
         GITHUB
       </a>
